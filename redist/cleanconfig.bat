277f90e7cd3874ff64cc57bec300941ab6e6e8c7
David Ralph <me@davidcralph.co.uk>
2022-12-09 10:41:08 +0000

    working copy, add missing redist files etc

--- redist/cleanconfig.bat
+++ redist/cleanconfig.bat
@@ -1,4 +1,8 @@
 del /Q /F dotnetfx35setup.exe
 del /Q /F dxwebsetup.exe
-del /Q /F redist_64/
+rmdir /Q /S redist_64
+rmdir /Q /S redist_32
+del /Q /F setfileassociation.bat
+del /Q /F checkdotnet.bat
+del /Q /F fileassociation.reg
 del %0
